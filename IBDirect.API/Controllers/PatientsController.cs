e26f8abc9191228226cdd3c670db58a6355360b4
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-02 22:24:43 +0100

    add patientscontroller

---
+++ IBDirect.API/Controllers/PatientsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace IBDirect.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PatientsController : ControllerBase
+{
+    public PatientsController(Parameters)
+    {
+        
+    }
+}
