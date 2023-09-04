b13a1aae2243f421de573f7e3f1064d8d44afab5
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-04 18:16:04 +0100

    add patients entity ]

---
+++ IBDirect.API/Entities/Patients.cs
@@ -0,0 +1,9 @@
+namespace IBDirect.API;
+
+public class Patients
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Pass { get; set; }
+    public string Salt { get; set; }
+}
