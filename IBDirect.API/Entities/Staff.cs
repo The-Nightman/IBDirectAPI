84dd8e840258f8e5cd8c7ea58d18be44722d118b
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-13 15:22:18 +0100

    add Staff entity

---
+++ IBDirect.API/Entities/Staff.cs
@@ -0,0 +1,12 @@
+namespace IBDirect.API.Entities;
+
+public class Staff
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Pass { get; set; }
+    public string Role { get; set; }
+    public string Hospital { get; set; }
+    public string Speciality { get; set; }
+    public string Salt { get; set; }
+}
