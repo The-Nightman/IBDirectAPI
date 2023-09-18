97468b29defea13f82d0541ad9e7c437c1404aff
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-19 00:05:44 +0100

    add field validation to RegisterDto

--- IBDirect.API/DTOs/RegisterDto.cs
+++ IBDirect.API/DTOs/RegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace IBDirect.API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string Name { get; set; }
+        
+        [Required]
         public string Password { get; set; }
     }
 }
