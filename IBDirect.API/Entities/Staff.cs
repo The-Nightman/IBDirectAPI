4c950e344618d96e5892baef1e98e2d8c741d185
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-19 16:10:54 +0100

    update staff entity to new schema model

--- IBDirect.API/Entities/Staff.cs
+++ IBDirect.API/Entities/Staff.cs
@@ -4,9 +4,6 @@ public class Staff
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Pass { get; set; }
-    public string Role { get; set; }
-    public string Hospital { get; set; }
-    public string Speciality { get; set; }
-    public string Salt { get; set; }
+    public byte[] PassHash { get; set; }
+    public byte[] Salt { get; set; }
 }
