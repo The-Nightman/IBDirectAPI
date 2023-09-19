1aaec98b7b83baa969e47910c26fb7c93b5c94e1
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-19 16:11:26 +0100

    add new migration UpdateStaffEntity

---
+++ IBDirect.API/Data/Migrations/20230919151017_UpdateStaffEntity.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBDirect.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateStaffEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hospital",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "Pass",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "Speciality",
+                table: "Staff");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Salt",
+                table: "Staff",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PassHash",
+                table: "Staff",
+                type: "bytea",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PassHash",
+                table: "Staff");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Salt",
+                table: "Staff",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Hospital",
+                table: "Staff",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Pass",
+                table: "Staff",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Staff",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Speciality",
+                table: "Staff",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
