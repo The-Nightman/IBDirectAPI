4dafc8848a829d72d846198c4039332271c9d1ec
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-24 16:09:17 +0100

    delete staff entity

--- IBDirect.API/Entities/Staff.cs
+++
@@ -1,9 +0,0 @@
-namespace IBDirect.API.Entities;
-
-public class Staff
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public byte[] PassHash { get; set; }
-    public byte[] Salt { get; set; }
-}
