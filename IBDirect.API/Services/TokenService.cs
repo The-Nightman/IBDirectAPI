16770b7e18c26b4c25d17e3c20efb00e48e1805c
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-26 18:57:58 +0100

    add tokenservice service class

---
+++ IBDirect.API/Services/TokenService.cs
@@ -0,0 +1,13 @@
+using IBDirect.API.Entities;
+using IBDirect.API.Interfaces;
+
+namespace IBDirect.API.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(Users user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
