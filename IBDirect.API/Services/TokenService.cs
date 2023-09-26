182d29c8547cfc481a8558bfdbb08a15e6b33826
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-09-26 22:56:32 +0100

    add TokenService logic with 24hr token expiry

--- IBDirect.API/Services/TokenService.cs
+++ IBDirect.API/Services/TokenService.cs
@@ -1,13 +1,40 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using IBDirect.API.Entities;
 using IBDirect.API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace IBDirect.API.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
         public string CreateToken(Users user)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.Name)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
