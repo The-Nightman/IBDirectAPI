36af274499e2a1dae92306b80eb8cf11d5b8a93c
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-10-04 18:04:35 +0100

    add PatientDetails entity

---
+++ IBDirect.API/Entities/PatientDetails.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace IBDirect.API.Entities;
+
+public class PatientDetails
+{
+    public int PatientId { get; set; }
+    public string Name { get; set; }
+    public string Hospital { get; set; }
+    public string Diagnosis { get; set; }
+    public bool Stoma { get; set; }
+    [MaxLength(2500)]
+    public string Notes { get; set; }
+    public int ConsultantId { get; set; }
+    public int NurseId { get; set; }
+    public int GenpractId { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string Address { get; set; }
+    public List<Appointment> Appointments { get; set; } = new();
+    public List<Survey> Surveys { get; set; } = new();
+    public List<Prescription> Prescriptions { get; set; } = new();
+}
