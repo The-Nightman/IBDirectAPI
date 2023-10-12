29aa60f89bff7b567d118d5332d9a26495063070
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-10-12 21:14:05 +0100

    add StaffDetailsDto

---
+++ IBDirect.API/DTOs/StaffDetailsDto.cs
@@ -0,0 +1,11 @@
+namespace IBDirect.API.DTOs
+{
+    public class StaffDetailsDto
+    {
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Speciality { get; set; }
+        public string Practice { get; set; }
+    }
+}
