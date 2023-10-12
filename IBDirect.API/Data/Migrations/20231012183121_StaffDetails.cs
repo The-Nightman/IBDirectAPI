7d6dbe1713e7354565e0c5aa4426871df3d24d70
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-10-12 20:04:48 +0100

    add StaffDetails to datacontext and new migration

---
+++ IBDirect.API/Data/Migrations/20231012183121_StaffDetails.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBDirect.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class StaffDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StaffDetails",
+                columns: table => new
+                {
+                    StaffId = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false),
+                    Speciality = table.Column<string>(type: "text", nullable: false),
+                    Practice = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StaffDetails", x => x.StaffId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StaffDetails");
+        }
+    }
+}
