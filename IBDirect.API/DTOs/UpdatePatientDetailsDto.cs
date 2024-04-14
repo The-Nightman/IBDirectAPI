7351cbb8f5d41a70c2321be074c2af309fbb714e
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-14 17:36:23 +0100

    add UpdatePatientDetailsDto

---
+++ IBDirect.API/DTOs/UpdatePatientDetailsDto.cs
@@ -0,0 +1,15 @@
+namespace IBDirect.API.DTOs
+{
+    public class UpdatePatientDetailsDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Hospital { get; set; }
+        public string Diagnosis { get; set; }
+        public DateOnly DiagnosisDate { get; set; }
+        public bool Stoma { get; set; }
+        public int ConsultantId { get; set; }
+        public int NurseId { get; set; }
+        public int StomaNurseId { get; set; }
+    }
+}
