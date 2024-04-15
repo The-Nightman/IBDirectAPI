179b15c6d7b05e3b85ff4d16d3a98218c2589015
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-15 21:10:31 +0100

    add addCors line

--- IBDirect.API/Program.cs
+++ IBDirect.API/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
