dd11b57856e307edc64c94da7aea14c270693b38
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-26 18:27:06 +0100

    add PresenceTracker to PresenceHub

--- IBDirect.API/SignalR/PresenceHub.cs
+++ IBDirect.API/SignalR/PresenceHub.cs
@@ -7,20 +7,42 @@ namespace IBDirect.API.SignalR
     [Authorize(Roles = "1,2,3,4,5")]
     public class PresenceHub : Hub
     {
+        private readonly PresenceTracker _tracker;
+
+        public PresenceHub(PresenceTracker tracker)
+        {
+            _tracker = tracker;
+        }
+
         public override async Task OnConnectedAsync()
         {
-            await Clients.All.SendAsync(
+            await _tracker.UserConnected(
+                Context.User.Claims.LastOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value,
+                Context.ConnectionId
+            );
+            await Clients.Others.SendAsync(
                 "UserIsOnline",
                 Context.User.Claims.LastOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value
             );
+
+            var currentUsers = await _tracker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+            await _tracker.UserDisconnected(
+                Context.User.Claims.LastOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value,
+                Context.ConnectionId
+            );
+
             await Clients.All.SendAsync(
                 "UserIsOffline",
                 Context.User.Claims.LastOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value
             );
+
+            var currentUsers = await _tracker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
 
             await base.OnDisconnectedAsync(exception);
         }
