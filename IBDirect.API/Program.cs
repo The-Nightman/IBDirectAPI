62d08169a14eb7e48fcc8fd485b7df8c2e66f684
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-26 01:06:52 +0100

    fix double slash caused by presence hub url

--- IBDirect.API/Program.cs
+++ IBDirect.API/Program.cs
@@ -37,7 +37,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-app.MapHub<PresenceHub>("/hubs/presence");
+app.MapHub<PresenceHub>("hubs/presence");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
