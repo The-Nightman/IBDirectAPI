38dd4507491bb2207eda8d36e153912ced109e62
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-04-28 20:13:26 +0100

    add SendMessage method to SignalR MessageHub

--- IBDirect.API/SignalR/MessageHub.cs
+++ IBDirect.API/SignalR/MessageHub.cs
@@ -1,7 +1,11 @@
 using System.Security.Claims;
+using IBDirect.API.Data;
+using IBDirect.API.DTOs;
+using IBDirect.API.Entities;
 using IBDirect.API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 
 namespace IBDirect.API.SignalR
 {
@@ -9,10 +13,12 @@ namespace IBDirect.API.SignalR
     public class MessageHub : Hub
     {
         private readonly IMessageRepository _messageRepository;
+        private readonly DataContext _context;
 
-        public MessageHub(IMessageRepository messageRepository)
+        public MessageHub(IMessageRepository messageRepository, DataContext context)
         {
             _messageRepository = messageRepository;
+            _context = context;
         }
 
         public override async Task OnConnectedAsync()
@@ -41,6 +47,44 @@ namespace IBDirect.API.SignalR
         public override Task OnDisconnectedAsync(Exception exception)
         {
             return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(MessageDto createMessageDto)
+        {
+            if (!await _context.Users.AnyAsync(u => u.Id == createMessageDto.SenderId))
+            {
+                throw new HubException("Sender does not exist");
+            }
+            else if (!await _context.Users.AnyAsync(u => u.Id == createMessageDto.RecipientId))
+            {
+                throw new HubException("Recipient does not exist");
+            }
+            else if (createMessageDto.SenderId == createMessageDto.RecipientId)
+            {
+                throw new HubException("Sender and recipient cannot be the same");
+            }
+
+            var message = new Message
+            {
+                Content = createMessageDto.Content,
+                DateSent = DateTime.UtcNow,
+                SenderId = createMessageDto.SenderId,
+                SenderName = createMessageDto.SenderName,
+                SenderRole = createMessageDto.SenderRole,
+                RecipientId = createMessageDto.RecipientId,
+                RecipientName = createMessageDto.RecipientName,
+                RecipientRole = createMessageDto.RecipientRole,
+                Read = false
+            };
+
+            _context.Messages.Add(message);
+            await _context.SaveChangesAsync();
+
+            var groupName = GetGroupName(
+                Context.User.Claims.LastOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value,
+                createMessageDto.RecipientId.ToString()
+            );
+            await Clients.Group(groupName).SendAsync("NewMessage", message);
         }
 
         public string GetGroupName(string caller, string other)
